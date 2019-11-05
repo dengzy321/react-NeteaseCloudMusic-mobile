160b7ab053d8e9983a407d6c05b780c10220103e
dengzy321 <1562922734@qq.com>
2019-11-05 11:27:49 +0800

    Header模块化

--- src/layouts/MainLayout/index.js
+++ src/layouts/MainLayout/index.js
@@ -1,43 +1,16 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom'
 import './index.css';
-import Drawer from '../../components/Drawer'
+import Header from '../../components/Header'
 
 class MainLayout extends React.Component {
-    state = {
-        showDrawer: false
-    }
-    changeShowStatus = () => {
-        this.setState(state => ({
-            showDrawer: !state.showDrawer
-        }))
-    }
+
     render() {
-        const tabs = [
-            { title: '我的', url: '/myCenter' },
-            { title: '发现', url: '/find' },
-            { title: '云村', url: '/square' },
-            { title: '视频', url: '/video' }
-        ];
         const { match, children } = this.props;
-        const { showDrawer } = this.state
         return (
             <div className='mainLayout'>
-                <div className='ml-header da'>
-                    <img className='menu_icon' src={require('../../assets/menu.png')} onClick={this.changeShowStatus}/>
-                    <div className='center dbc'>
-                        {
-                            tabs.map((item, index) => {
-                                return <NavLink activeClassName='selected' key={index} to={item.url}>{item.title}</NavLink>
-                            })
-                        }
-                    </div>
-                    <img className='search_icon' src={require('../../assets/search.png')} />
-                </div>                                                          
+                <Header />
                 {children}
-
-                {/* 显示Drawer */}
-                {showDrawer && <Drawer showDrawer={showDrawer} changeShowStatus={this.changeShowStatus}/>}
+                
             </div>
         );
     }
